d82537edfa334b5fc1bbc6653f7b4b52080f19e0
AlchazaR <VLAD.ZMTR@gmail.com>
2020-01-03 20:34:02 +0200

    Update db_connect.php

--- application/configs/db_connect.php
+++ application/configs/db_connect.php
@@ -1,4 +1,4 @@
 <?php
-$db = mysql_connect("localhost", "root", "");
-mysql_select_db("armi_intranet", $db) or die(mysql_errno() . ": " . mysql_error() . "<br>");
+$db = mysql_connect("localhost", "****", "****");
+mysql_select_db("****", $db) or die(mysql_errno() . ": " . mysql_error() . "<br>");
 ?>
